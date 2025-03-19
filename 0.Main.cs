28b015ced80528b57742a4ed6aff79b4c01ca885
Saramsha <saramsha2004@gmail.com>
2025-03-19 19:10:30 +0545

    Basic Stuff

---
+++ 0.Main.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1;
+using DotNetLab;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pattern task1 = new Pattern();
+        task1.create();
+
+
+
+    }
+}
---
+++ 2.Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Calculator
+    {
+        public void calc()
+        {
+
+            Console.Write("Enter Num 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Num 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose +, -, *, /");
+            string op = Console.ReadLine();
+
+            int result = 0;
+            if (op == "+")
+                result = num1 + num2;
+            else if (op == "-")
+                result = num1 - num2;
+            else if (op == "*")
+                result = num1 * num2;
+            else if (op == "/")
+                result = num1 / num2;
+            else
+                Console.WriteLine("Enter a valid op");
+
+            //switch (op)
+            //{
+            //    case "+":
+            //        result = num1 + num2;
+            //        break;
+            //    case "-":
+            //        result = num1 - num2;
+            //        break;
+            //    case "*":
+            //        result = num1 * num2;
+            //        break;
+            //    case "/":
+            //        result = num1 / num2;
+            //        break;
+            //    default:
+            //        Console.WriteLine("Enter a valid op");
+            //        break;
+            //}
+
+            Console.WriteLine(num1 + " " + op + " " + num2 + " = " + result);
+        }
+    }
+}
